30a412a7919fb292f667e4c002015d45da0e8369
Leonardo F.C <leonardofc2002@gmail.com>
2024-08-06 14:32:21 -0300

    chore: add github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -23,10 +23,11 @@ export default defineConfig({
   ],
   build: {
     // Tauri uses Chromium on Windows and WebKit on macOS and Linux
-    target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
+    target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari14",
     // don't minify for debug builds
     minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
     // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_DEBUG,
   },
+  base: process.env.BASE_PATH,
 });
